b641273c52c2b4d61187fb272d36ba0c3ecdfc42
Jesper Holm <jesperliholm@gmail.com>
2022-11-22 11:43:33 +0100

    Show Card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, timer, Subscription } from 'rxjs';
+import { Card } from "./card";
 
 @Component({
   selector: 'app-root',
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Card } from '../card';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  @Input() card!: Card; //Not nice, fix this
+  constructor() {
+
+  }
+
+  getBackgroundImage(item: Card) {
+    if (this.card.selected === true) {
+       // let backgroundUrl = 'url(assets/img/'+ item.card1 + '.jpg)'
+    const backgroundUrl = `url(assets/img/${item.card1}.jpg)`;
+    return backgroundUrl;
+    } else {
+      return '';
+    } 
+  }
 
   ngOnInit(): void {
+    this.card.selected = false;
   }
 
 }
